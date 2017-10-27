ea2cfd33eadb825c9fb23fc2995b483055a71a9c
Alina Lei <573493657@qq.com>
2017-10-27 17:15:13 +0800

    v1.0.29

--- bi_model_service-master/Webserver/comm/static/js/translate.js
+++ bi_model_service-master/Webserver/comm/static/js/translate.js
@@ -11,7 +11,7 @@ stack={DW_predict_record_accuracy: "BI预测模型评估", DW_predictmodel_statu
 	};
 col_stack={ptype:"预测类型","count(1)":"总记录数",uid:"用户id",stid:"油站id",group_id:"集团id",predict_dt:"预测间隔",update_time:"更新时间",activity_id:"活动id"
 		  ,orderdate:"订单时间:日",ordermonth:"订单时间:月",splitdate:"分摊时间:日",splitmonth:"分摊时间:月",splitmoney:"分摊金额",typedesc:"产品类型(简述)",desc_:"产品类型(简述)"
-		  ,monthdays:"月分摊天数",cmcc.mobilearea:"地市",sm.addtime:"注册时间"
+		  ,monthdays:"月分摊天数"
 	}
 con_stack={"orderTime >= ":"YYYY-MM(-DD HH:mm:SS)","orderTime <= ":"YYYY-MM(-DD HH:mm:SS)","splittype in ":"'n+ ','S-'","orderId in ":"多个订单号,隔开"
 		  ,"shopshortname in ":"'上海A','深圳B'","shopshortname like ":"填入包含的关键字即可"
