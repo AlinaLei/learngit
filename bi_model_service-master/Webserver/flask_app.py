274553880b4b76ea9032058415bf1976c21c58f3
Alina Lei <573493657@qq.com>
2017-10-30 18:35:11 +0800

    v1.0.41

--- bi_model_service-master/Webserver/flask_app.py
+++ bi_model_service-master/Webserver/flask_app.py
@@ -326,6 +326,7 @@ def v_table():
     proc = subprocess.Popen("/usr/bin/head -n 999 "+pat, shell=True, stdout=subprocess.PIPE)
     cod = "utf8" if cod is None else cod
     res, errs = proc.communicate(timeout=15)
+    #red = res.decode().split('\n')
     lines = [line.split("\t") for line in str(res,cod).split("\n")[:-1]] if errs is None else []
     ss = render_template("v_tab_uu.html", base_dict=WBASE, lines=lines, xlsn=pat, tas=wi.tas)
     return ss
